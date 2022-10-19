4084948b1f56a86aaee2412f434a8dde908ddb3b
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-10-19 17:44:19 -0400

    fix: add hook for fetching backend

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,34 +1,40 @@
-import { useState, useEffect } from "react";
+import useFetchData from "./utilities/hooks/useFetchData";
 import NavBar from "./components/NavBar";
 import DashTop from "./components/DashTop";
 import DashBottom from "./components/DashBottom";
-import type { PassingData } from "./types/dataTypes";
 
-function App() {
-  const [allPassingData, setAllPassingData] = useState<PassingData[] | null>(
-    null
-  );
-  console.log(allPassingData);
+const App = () => {
+  const { data, loading, error } = useFetchData();
 
-  useEffect(() => {
-    fetch("/passing")
-      .then((res) => res.json())
-      .then((data) => {
-        setAllPassingData(data.data);
-      })
-      .catch((err) => console.error(err));
-  }, []);
+  let conditionalContent;
+
+  if (loading) {
+    conditionalContent = (
+      <div className="w-full h-full bg-white p-12 flex items-center justify-center">
+        <p className="text-2xl">Loading...</p>
+      </div>
+    );
+  } else if (!loading && !error) {
+    conditionalContent = (
+      <div className="w-full h-full bg-white p-12">
+          <DashTop allPassingData={data} />
+          <DashBottom allPassingData={data} />
+        </div>
+    )
+  } else if (!loading && error) {
+    conditionalContent = (
+      <div className="w-full h-full bg-white p-12 flex items-center justify-center">
+        <p className="text-2xl">{error}</p>
+      </div>
+    )
+  }
 
   return (
     <div className="flex h-screen w-full font-montserrat text-[#1f1f1f]">
       <NavBar />
-
-      <div className="w-full h-full bg-white p-12">
-        <DashTop allPassingData={allPassingData} />
-        <DashBottom allPassingData={allPassingData} />
-      </div>
+      {conditionalContent}
     </div>
   );
-}
+};
 
 export default App;
