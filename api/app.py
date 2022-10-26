4ffe5834228dbc36094858f2320f8a93cd3b8dfc
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-10-26 14:07:41 -0400

    fix: update app.py success message

--- api/app.py
+++ api/app.py
@@ -1,38 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 import pandas as pd
 import nfl_data_py as nfl
-import math
 
 app = Flask(__name__)
 
-# print('test')
-# n1 = math.nan
-# print(n1)
-# print(math.isnan(n1))
-
-# passingDf = nfl.import_ngs_data(stat_type='passing')
-# passingList = passingDf.to_dict("records")
-# passingTest = [passingList[0], passingList[1]]
-# for pass_item in passingTest:
-        # print(pass_item)
-        # for key in pass_item:
-            # print(key)
-            # print(pass_item[key])
-            # if (pass_item[key] == 'Matt Ryan'):
-            #     print(pass_item[key])
-            #     pass_item[key] = 'Charlie Ryan'
-            #     print(pass_item[key])
-
+@app.route('/')
+def index():
+    return jsonify({"Status": "Flask App is Online"})
 
 @app.route('/passing')
-def index():
+def passing():
     # create a variable to store output from a nfl method (testing data)
     passingDf = nfl.import_ngs_data(stat_type='passing')
     # convert from pandas df to list of dictionaries
     passingList = passingDf.to_dict("records")
-
-    # TEST returning one item
-    # passingTest = [passingList[0], passingList[1], passingList[2], passingList[3], passingList[4], passingList[5]]
 
     def isNaN(num):
         return num != num
