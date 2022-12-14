33d71ef150eedfdf931a9e2b0d6127f1d14a7e40
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-12-13 19:35:15 -0500

    data: add rushing data to mongo

--- server/models/Rusher.js
+++ server/models/Rusher.js
@@ -1,72 +1,72 @@
 const mongoose = require("mongoose");
 
 const RusherSchema = new mongoose.Schema({
-    avg_rush_yards: {
-        type: Number,
-    },
-      avg_time_to_los: {
-        type: Number,
-    },
-      efficiency: {
-        type: Number,
-    },
-      expected_rush_yards: {
-        type: Number,
-    },
-      percent_attempts_gte_eight_defenders: {
-        type: Number,
-    },
-      player_display_name: {
-        type: String,
-    },
-      player_first_name:  {
-        type: String,
-    },
-      player_gsis_id: {
-        type: String,
-    },
-      player_jersey_number: {
-        type: Number,
-    },
-      player_last_name:  {
-        type: String,
-    },
-      player_position: {
-        type: String,
-    },
-      player_short_name: {
-        type: String,
-    },
-      rush_attempts: {
-        type: Number,
-    },
-      rush_pct_over_expected: {
-        type: Number,
-    },
-      rush_touchdowns: {
-        type: Number,
-    },
-      rush_yards: {
-        type: Number,
-    },
-      rush_yards_over_expected: {
-        type: Number,
-    },
-      rush_yards_over_expected_per_att: {
-        type: Number,
-    },
-      season: {
-        type: Number,
-    },
-      season_type:  {
-        type: String,
-    },
-      team_abbr:  {
-        type: String,
-    },
-      week: {
-        type: Number,
-    },
+  avg_rush_yards: {
+    type: Number,
+  },
+  avg_time_to_los: {
+    type: Number,
+  },
+  efficiency: {
+    type: Number,
+  },
+  expected_rush_yards: {
+    type: Number,
+  },
+  percent_attempts_gte_eight_defenders: {
+    type: Number,
+  },
+  player_display_name: {
+    type: String,
+  },
+  player_first_name: {
+    type: String,
+  },
+  player_gsis_id: {
+    type: String,
+  },
+  player_jersey_number: {
+    type: Number,
+  },
+  player_last_name: {
+    type: String,
+  },
+  player_position: {
+    type: String,
+  },
+  player_short_name: {
+    type: String,
+  },
+  rush_attempts: {
+    type: Number,
+  },
+  rush_pct_over_expected: {
+    type: Number,
+  },
+  rush_touchdowns: {
+    type: Number,
+  },
+  rush_yards: {
+    type: Number,
+  },
+  rush_yards_over_expected: {
+    type: Number,
+  },
+  rush_yards_over_expected_per_att: {
+    type: Number,
+  },
+  season: {
+    type: Number,
+  },
+  season_type: {
+    type: String,
+  },
+  team_abbr: {
+    type: String,
+  },
+  week: {
+    type: Number,
+  },
 });
 
 module.exports = mongoose.model("Rusher", RusherSchema);
--- server/seeder.js
+++ server/seeder.js
@@ -2,28 +2,28 @@ const fs = require("fs");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
-const Passer = require("./models/Passer");
-// const Rusher = require(".models/Rusher");
+// const Passer = require("./models/Passer");
+const Rusher = require("./models/Rusher");
 
 // Connect to DB
 connectDB();
 
 // Passing Data
-const passers = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/passing.json`, "utf-8")
-);
+// const passers = JSON.parse(
+//   fs.readFileSync(`${__dirname}/_data/passing.json`, "utf-8")
+// );
 
 // Rushing Data
-// const rushers = JSON.parse(
-//   fs.readFileSync(`${__dirname}/_data/rushing.json`, "utf-8")
-// );
+const rushers = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/rushing.json`, "utf-8")
+);
 
 
 // import data to DB
 const importData = async () => {
   try {
     // flip the model and data input depending on which set you are trying to update
-    await Passer.create(passers);
+    await Rusher.create(rushers);
     console.log("Data Imported");
     process.exit();
   } catch (err) {
