bd74249d7a29bf37df660557b50d69fe4e5639c5
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-12-31 23:32:14 -0500

    feat: add mobile screen view for landing pg

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -13,29 +13,29 @@ const LandingPage = () => {
   return (
     <div className="relative w-screen h-screen flex-col justify-between bg-gray-50 text-black">
       {/* Horizontal (Landing Page) Nav Bar Component */}
-      <div className="w-full h-1/6 flex justify-between items-center border px-20">
-        <p className="ml-10 font-bold text-2xl">NFL NEXT GEN DASH</p>
+      <div className="w-full h-1/6 flex justify-between items-center border px-5 lg:px-20">
+        <p className="ml-10 font-bold text-lg lg:text-2xl">NFL NEXT GEN DASH</p>
         <button
-          className="bg-[#0b6241] rounded-lg px-6 py-3 text-sm text-white"
+          className="bg-[#0b6241] rounded-lg px-3 lg:px-6 py-2 lg:py-3 text-sm text-white"
           onClick={btnHandler}
         >
           View Dashboard
         </button>
       </div>
 
-      <div className="relative h-4/6 w-full flex px-20 pt-14 overflow-hidden">
+      <div className="relative h-4/6 w-full flex px-5 lg:px-20 pt-14 overflow-hidden">
         <div className="absolute inset-1/4 -translate-y-2/4 translate-x-2/4 w-1/5 h-3/4 bg-slate-500 rounded-full mix-blend-multiply filter blur-3xl opacity-60"></div>
         <div className="absolute inset-2/4 -translate-y-4/4 -translate-x-2/4 w-1/3 h-3/4 bg-green-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30"></div>
         <div className="absolute inset-1/4 -translate-y-1/2 translate-x-full w-1/3 h-1/2 bg-green-500 rounded-full mix-blend-multiply filter blur-3xl opacity-60"></div>
         <div className="absolute top-[200px] right-0 w-1/3 h-1/2 bg-slate-500 rounded-full mix-blend-multiply filter blur-3xl opacity-60"></div>
         {/* <AiTwotoneTrophy className="w-40 h-40 absolute inset-2/4 -translate-y-full -translate-x-2/4 text-white text-3xl" /> */}
 
-        <div className="h-full w-1/2 pl-10">
+        <div className="h-full w-full lg:w-1/2 pl-10">
           <p className="w-fit text-xs border rounded-lg font-light px-2 py-1">
             <span className="mr-[5px]">&#127944;</span>TOP NFL DASH WEB APP
           </p>
 
-          <div className="text-4xl mt-2 mb-4">
+          <div className="text-3xl lg:text-4xl mt-2 mb-4 font-semibold">
             <h1 className="mb-2">The best way</h1>
             <h1 className="w-fit bg-black text-white px-3 py-1 rounded-lg mb-2 -rotate-6">
               to track
@@ -43,7 +43,7 @@ const LandingPage = () => {
             <h1 className="mb-2">your fantasy team</h1>
           </div>
 
-          <p className="font-light mb-4">
+          <p className="font-light mb-4 text-sm lg:text-base">
             NFL Next Gen Dash allows you to gain insights <br /> by leveraging
             AWS Next Gen Stats data
           </p>
@@ -57,7 +57,7 @@ const LandingPage = () => {
         </div>
       </div>
 
-      <div className="h-[350px] w-[625px] p-3 rounded-2xl absolute bg-white border top-[145px] right-20">
+      <div className="hidden xl:block h-[350px] w-[625px] p-3 rounded-2xl absolute bg-white border top-[115px] right-20">
         <img
           src={AppFullDark}
           alt="Dashboard Full Dark"
@@ -66,7 +66,7 @@ const LandingPage = () => {
       </div>
 
       {/* bg-stone-800 */}
-      <div className="h-[230px] w-[180px] p-2 rounded-2xl  bg-[#0b6241] absolute top-[355px] right-40">
+      <div className="hidden xl:block h-[230px] w-[180px] p-2 rounded-2xl  bg-[#0b6241] absolute xl:top-[325px] xl:right-40">
         <img
           src={AppMobileLight}
           alt="Dashboard Mobile Light"
@@ -75,7 +75,7 @@ const LandingPage = () => {
         />
       </div>
 
-      <div className="w-full flex items-center h-1/6 pl-28 gap-24 font-semibold text-3xl text-black">
+      <div className="w-full flex items-center h-1/6 pl-16 lg:pl-28 gap-24 font-semibold text-xl lg:text-3xl text-black">
         <div className="flex items-center">
           <p>30+</p>
           <p className="text-xs font-light ml-4">
