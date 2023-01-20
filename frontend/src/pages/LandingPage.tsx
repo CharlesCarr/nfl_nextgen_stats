dfa69fef4b5bf89b6b9d370c14726dff99d910f6
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2023-01-19 20:09:24 -0500

    fix(landing): adjust image positioning

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -17,7 +17,23 @@ const LandingPage = () => {
         <div className="absolute inset-2/4 -translate-y-4/4 -translate-x-2/4 w-1/3 h-3/4 bg-green-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30"></div>
         <div className="absolute inset-1/4 -translate-y-1/2 translate-x-full w-1/3 h-1/2 bg-green-500 rounded-full mix-blend-multiply filter blur-3xl opacity-60"></div>
         <div className="absolute top-[200px] right-0 w-1/3 h-1/2 bg-slate-500 rounded-full mix-blend-multiply filter blur-3xl opacity-60"></div>
-        {/* <AiTwotoneTrophy className="w-40 h-40 absolute inset-2/4 -translate-y-full -translate-x-2/4 text-white text-3xl" /> */}
+
+        <div className="hidden lg:block h-[280px] w-[515px] xl:h-[350px] xl:w-[625px] p-3 rounded-2xl absolute bg-white border top-0 bottom-0 my-auto mx-0 right-10">
+          <img
+            src={AppFullDark}
+            alt="Dashboard Full Dark"
+            className="w-full h-full rounded-2xl"
+          />
+        </div>
+
+        <div className="hidden md:block lg:hidden xl:block md:h-[300px] md:w-[220px] xl:h-[230px] xl:w-[180px] p-2 rounded-2xl  bg-[#0b6241] absolute top-0 xl:top-6 bottom-0 my-auto mx-0 right-32 xl:right-5">
+          <img
+            src={AppMobileLight}
+            alt="Dashboard Mobile Light"
+            //   border-2 border-black
+            className="w-full h-full rounded-2xl"
+          />
+        </div>
 
         <div className="h-full w-full lg:w-1/2 pl-10">
           <p className="w-fit text-xs border rounded-lg font-light px-2 py-1">
@@ -39,24 +55,6 @@ const LandingPage = () => {
 
           <ViewDashBtn />
         </div>
-      </div>
-
-      <div className="hidden lg:block h-[280px] w-[515px] xl:h-[350px] xl:w-[625px] p-3 rounded-2xl absolute bg-white border top-[145px] right-10 xl:top-[145px] xl:right-20">
-        <img
-          src={AppFullDark}
-          alt="Dashboard Full Dark"
-          className="w-full h-full rounded-2xl"
-        />
-      </div>
-
-
-      <div className="hidden md:block lg:hidden xl:block md:h-[300px] md:w-[220px] xl:h-[230px] xl:w-[180px] p-2 rounded-2xl  bg-[#0b6241] absolute md:top-[150px] md:right-48 xl:top-[355px] xl:right-40">
-        <img
-          src={AppMobileLight}
-          alt="Dashboard Mobile Light"
-          //   border-2 border-black
-          className="w-full h-full rounded-2xl"
-        />
       </div>
 
       <div className="w-full flex items-center h-1/6 pl-16 lg:pl-28 gap-24 font-semibold text-xl lg:text-3xl text-black">
