1c76660c61f650d6e33f8d94a9b28bb06c13a885
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2023-01-23 10:27:57 -0500

    fix(loader): make loader smaller

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,7 +1,7 @@
 import ReactLoading from "react-loading";
 
 const Loading = () => (
-  <ReactLoading type={"spin"} color={"#0b6241"} height={"100px"} width={"100px"} />
+  <ReactLoading type={"spin"} color={"#0b6241"} height={"50px"} width={"50px"} />
 );
 
 export default Loading;
